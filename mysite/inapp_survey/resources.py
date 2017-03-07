8fa0dd4e7c7fa138962e01c881ae82051889ab56
Jaimin Patel <jaimin9999@gmail.com>
2017-03-06 23:59:04 -0500

    placeholders

--- mysite/inapp_survey/resources.py
+++ mysite/inapp_survey/resources.py
@@ -5,6 +5,8 @@ from rest_framework.response import Response
 from inapp_survey.models import Campaign
 from inapp_survey.serializers import CampaignSerializer, CampaignListSerializer
 
+# TODO: Return the active campaign based on the information is passed in querystring
+# param - will need to return based on that the first one.
 class ActiveCampaignList(generics.ListAPIView):
 
     model = Campaign
@@ -13,6 +15,7 @@ class ActiveCampaignList(generics.ListAPIView):
     permission_classes = [
         permissions.AllowAny
     ]
+
 
 class ActiveCampaignDetails(generics.RetrieveAPIView):
     """Get the campaign details"""
@@ -24,3 +27,7 @@ class ActiveCampaignDetails(generics.RetrieveAPIView):
     ]
     lookup_field = 'slug'
     queryset = Campaign.objects.all()
+
+
+# resource to make user campaign
+# resource to make user campaign answers
