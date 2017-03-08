9428b8379046859338d44853f5e318ea7715ab5e
Jaimin Patel <jaimin9999@gmail.com>
2017-03-08 00:33:01 -0500

    added steps for the campaign

--- mysite/inapp_survey/serializers.py
+++ mysite/inapp_survey/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
 
-from inapp_survey.models import Campaign
+from inapp_survey.models import Campaign, CampaignCustomParam, \
+    CampaignQuestion
+
+
+class CampaignCustomParamSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = CampaignCustomParam
+        fields = (
+            "param_key",
+            "param_value",
+        )
 
 
 class CampaignListSerializer(serializers.ModelSerializer):
@@ -17,7 +28,26 @@ class CampaignListSerializer(serializers.ModelSerializer):
             'campaign_type',
         )
 
+
+class CampaignQuestionSerializer(serializers.ModelSerializer):
+
+        class Meta:
+            model = CampaignQuestion
+            fields = (
+                "id",
+                "question",
+                "order",
+            )
+
+
 class CampaignSerializer(serializers.ModelSerializer):
+
+        custom_params = CampaignCustomParamSerializer(
+            many=True, read_only=True)
+
+        steps = CampaignQuestionSerializer(
+            source='questions',
+            many=True)
 
         class Meta:
             model = Campaign
@@ -28,4 +58,7 @@ class CampaignSerializer(serializers.ModelSerializer):
                 'is_authenticated',
                 'expiry_date',
                 'campaign_type',
+                'steps',
+                'custom_params'
             )
+
