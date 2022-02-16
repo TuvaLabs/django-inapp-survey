b82da5b0177d1fbc6c620f36197aafba9effba1c
Naveenkumar Ganesan <nganesan@tuvalabs.com>
2022-02-16 13:58:28 +0530

    Fixing template path

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['/Users/Jaimin/Apps/opensource/django-inapp-survey/mysite/templates/'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
